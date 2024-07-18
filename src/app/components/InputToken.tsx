f14ed1d2e4efaf32d93c25448e912d81d4d71a47
ikhwanhsn <ikhwanulhusna111@gmail.com>
2024-07-18 11:42:03 +0700

    chore: add input feature

--- src/app/components/InputToken.tsx
+++ src/app/components/InputToken.tsx
@@ -1,15 +1,101 @@
+"use client";
+
+import { useState } from "react";
+
 const InputToken = () => {
+  const [tokenAddress, setTokenAddress] = useState("");
+  const [isTokenActive, setIsTokenActive] = useState(false);
+  const dataTools = [
+    {
+      name: "Dexscreener",
+      url: `https://dexscreener.com/solana/${tokenAddress}`,
+    },
+    {
+      name: "Dextools",
+      url: `https://www.dextools.io/app/en/solana/pair-explorer/${tokenAddress}`,
+    },
+    {
+      name: "Solscan",
+      url: `https://solscan.io/token/${tokenAddress}`,
+    },
+    {
+      name: "Rugcheck",
+      url: `https://rugcheck.xyz/tokens/${tokenAddress}`,
+    },
+    {
+      name: "Bubblemaps",
+      url: `https://app.bubblemaps.io/sol/token/${tokenAddress}`,
+    },
+  ];
+  const dataExchanges = [
+    {
+      name: "Raydium",
+      url: `https://raydium.io/swap/?inputMint=sol&outputMint=${tokenAddress}`,
+    },
+    {
+      name: "Jupiter",
+      url: `https://jup.ag/swap/SOL-${tokenAddress}`,
+    },
+    {
+      name: "Photon",
+      url: `https://photon-sol.tinyastro.io/en/lp/${tokenAddress}`,
+    },
+  ];
+  const track = () => {
+    if (!tokenAddress) {
+      alert("Please enter token address");
+      return;
+    }
+    if (tokenAddress.length !== 44) {
+      alert("Please enter valid token address");
+      return;
+    }
+    setIsTokenActive(true);
+  };
+  const clear = () => {
+    setTokenAddress("");
+    setIsTokenActive(false);
+  };
+
   return (
     <main className=" bg-background text-text mt-24 w-fit mx-auto">
-      <section className="space-x-1 mb-5">
+      <section className="space-x-1 mb-4 mx-auto w-fit">
         <input
           type="text"
           placeholder="Paste your token address here..."
+          onChange={(e) => setTokenAddress(e.target.value)}
+          value={tokenAddress}
           className="input w-96 text-text input-bordered bg-white"
         />
-        <button className="btn btn-primary text-white">Track</button>
-        <button className="btn btn-error text-white">Clear</button>
+        <button className="btn btn-primary text-white" onClick={track}>
+          Track
+        </button>
+        <button className="btn btn-error text-white" onClick={clear}>
+          Clear
+        </button>
       </section>
+      {isTokenActive && (
+        <section className="mx-auto space-x-1 w-fit">
+          {dataTools.map((item, index) => (
+            <a key={index} href={item.url} target="_blank">
+              <button className="btn btn-warning text-white w-28">
+                {item.name}
+              </button>
+            </a>
+          ))}
+        </section>
+      )}
+      {isTokenActive && (
+        <section className="mx-auto w-fit space-x-1 mt-2">
+          {dataExchanges.map((item, index) => (
+            <a key={index} href={item.url} target="_blank">
+              <button className="btn btn-info text-white w-28">
+                {item.name}
+              </button>
+            </a>
+          ))}
+        </section>
+      )}
     </main>
   );
 };
